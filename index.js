b7b447c07bf5a44bf37c453894b1d18be5488882
Maria Dayekh <mariaaldayekh@gmail.com>
2022-12-18 18:54:08 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2015", result: "W"},
+    {year: "2016", result: "L"}
+
+  ];
+
+function findWin (element) {
+
+ return element.result === "W";
+};
+/* 
+function superbowlWin (element) {
+
+    let winningRecord = record.find ((element) => findWin (element));
+     return winningRecord.year 
+    };
+*/
+    function superbowlWin (record){
+let winningRecord = record.find(({result}) => result === "W");
+
+return(winningRecord.year); }
